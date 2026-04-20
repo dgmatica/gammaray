cd627ef4f8910523e68de39168d0f5902800a058
dgmatica <denisx1672@gmail.com>
2026-04-21 00:27:36 +0300

    Update naming for returned_items count

--- backend/main.py
+++ backend/main.py
@@ -31,22 +31,37 @@ def root():
 def get_logs(
     limit: Annotated[int, Query(ge=1, le=15000)] = 5000,
 ):
-    logs, failed_logs = split_logs()
-    return {"total": len(logs), "data": logs[-limit:]}
+    logs, _ = split_logs()
+    data = logs[-limit:]
+
+    return {
+        "returned_items": len(data),
+        "data": data,
+    }
 
 
 @app.get("/failed-logs")
 def get_failed_logs(
     limit: Annotated[int, Query(ge=1, le=15000)] = 5000,
 ):
-    logs, failed_logs = split_logs()
-    return {"total": len(failed_logs), "data": failed_logs[-limit:]}
+    _, failed_logs = split_logs()
+    data = failed_logs[-limit:]
+
+    return {
+        "returned_items": len(data),
+        "data": data,
+    }
 
 
 @app.get("/stats/status-codes")
 def get_status_codes():
     logs, _ = split_logs()
-    return {"total": len(logs), "data": get_status_counts(logs)}
+    data = get_status_counts(logs)
+
+    return {
+        "returned_items": len(data),
+        "data": data,
+    }
 
 
 @app.get("/stats/top-pages")
@@ -54,7 +69,12 @@ def get_top_pages_stats(
     limit: Annotated[int, Query(ge=1, le=1000)] = 100,
 ):
     logs, _ = split_logs()
-    return {"total": len(logs), "data": get_top_pages(logs)[:limit]}
+    data = get_top_pages(logs)[:limit]
+
+    return {
+        "returned_items": len(data),
+        "data": data,
+    }
 
 
 @app.get("/stats/top-size-pages")
