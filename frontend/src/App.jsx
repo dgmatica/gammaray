321b3168520b15ac69b2d2a23a469cd5c330213f
dgmatica <denisx1672@gmail.com>
2026-04-28 16:27:51 +0300

    Add proto-styling + placeholders for endpoints

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,26 @@ function App() {
       <h1 className="page-title">SYNTHROPY DASHBOARD</h1>
 
       <OverviewSection />
+
+      <section className="section">
+        <h2 className="section-title section-top-pages">Top Pages</h2>
+        <p>Placeholder</p>
+      </section>
+
+      <section className="section">
+        <h2 className="section-title section-status-codes">Status Codes</h2>
+        <p>Placeholder</p>
+      </section>
+
+      <section className="section">
+        <h2 className="section-title section-top-sizes">Top Size Pages</h2>
+        <p>Placeholder</p>
+      </section>
+
+      <section className="section">
+        <h2 className="section-title section-method-status">Method Status</h2>
+        <p>Placeholder</p>
+      </section>
     </div>
   )
 }
